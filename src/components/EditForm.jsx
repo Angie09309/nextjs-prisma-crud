958b9b390ff7bb87423d3a192f0ae8b1ad27b9da
Angie Barreto <angiepbg13@gmail.com>
2025-07-25 21:06:40 -0500

    Añadir botón de eliminación en EditForm

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -72,9 +72,31 @@ export const EditForm = ({ id }) => {
                 value={description}
             ></textarea>
 
-            <button className="bg-blue-500 hover:bg-blue-700 text-white  font-bold py-2 px-4 ounded">
-                Crear
-            </button>
+            <div className='flex justify-between'>
+                <button type='submit'
+                    className="bg-blue-500 hover:bg-blue-700 text-white  font-bold py-2 px-4 ounded">
+                    Crear
+                </button>
+
+                {
+                    id && (
+                        <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded '
+                            type='button'
+                            onClick={async () => {
+                                const res = await fetch(`/api/tasks/${id}`, {
+                                    method: "DELETE"
+                                })
+                                const data = await res.json()
+                                router.refresh()
+                                router.push("/")
+                            }}>
+
+                            Delete
+                        </button>
+                    )
+                }
+            </div>
+
         </form>
     )
 }
